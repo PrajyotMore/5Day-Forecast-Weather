b13a1020270a68f14a3f4752779adedb00d872c0
PrajyotMore <76533468+PrajyotMore@users.noreply.github.com>
2021-05-04 04:49:33 +0530

    Changes done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CitySelect from './Components/CitySelect';
-import {API_KEY, API_BASE_URL} from './Api/config';
 import WeatherList from './Components/WeatherList';
 import {Route,Switch} from 'react-router-dom';
 import Error from './Error';
@@ -9,12 +8,9 @@ const App = () => {
   return (
     <>
     <Switch>
-      <Route exact path='/'/>
+      <Route exact path='/' component={CitySelect}/>
       <Route component={Error}/>
     </Switch>
-    <div className="App">
-    <CitySelect/>
-    </div>
     </>
   );
 }
